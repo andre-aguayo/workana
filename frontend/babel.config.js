83308e75868d6eff4922013cd05aeca0ae72a5e2
Andre Aguayo <andre.aguayo@hotmail.com>
2023-04-02 04:45:19 -0300

    babel lint disable

--- frontend/babel.config.js
+++ frontend/babel.config.js
@@ -1,1 +1,37 @@
-export const presets = ["@vue/cli-plugin-babel/preset"];
+export default function (api) {
+  api.cache(true);
+  api.assertVersion("^7.4.5");
+
+  const presets = [
+    [
+      "@babel/preset-env",
+      {
+        targets: {
+          esmodules: true,
+          node: true,
+        },
+      },
+    ],
+  ];
+  const plugins = [
+    ["@babel/plugin-transform-modules-commonjs"],
+    ["@babel/plugin-transform-destructuring"],
+    ["@babel/plugin-proposal-class-properties"],
+    [
+      "@babel/plugin-proposal-decorators",
+      {
+        decoratorsBeforeExport: true,
+      },
+    ],
+    ["@babel/plugin-proposal-export-namespace-from"],
+    ["@babel/plugin-proposal-object-rest-spread"],
+    ["@babel/plugin-transform-template-literals"],
+    ["@babel/plugin-transform-runtime"],
+    ["@babel/plugin-transform-classes"],
+  ];
+
+  return {
+    presets,
+    plugins,
+  };
+}
