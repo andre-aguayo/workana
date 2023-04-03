959868b093919804fe1a4a729df24148b8a09430
Andre Aguayo <andre.aguayo@hotmail.com>
2023-04-03 01:34:57 -0300

    Create sales table

--- devops/tables.sql
+++ devops/tables.sql
@@ -5,8 +5,7 @@ CREATE TABLE product_categories (
     tax INT
 );
 
-CREATE INDEX idx_product_categories
-ON product_categories (id, name);
+CREATE INDEX idx_product_categories ON product_categories (id, name);
 
 CREATE TABLE products (
     id INT NOT NULL AUTO_INCREMENT PRIMARY KEY,
@@ -16,5 +15,13 @@ CREATE TABLE products (
     category_id INT,
     FOREIGN KEY (category_id) REFERENCES product_categories(id)
 );
-CREATE INDEX idx_products
-ON products (id, name, category_id);
+
+CREATE INDEX idx_products ON products (id, name, category_id);
+
+CREATE TABLE sales (
+    id INT NOT NULL AUTO_INCREMENT PRIMARY KEY,
+    value INT,
+    tax INT,
+    created_at DATE DEFAULT (CURRENT_DATE)
+);
+CREATE INDEX idx_sales ON sales (id, created_at);
