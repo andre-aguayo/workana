b2ff0345998fe3b5885b7d5706702fcf38c75546
Andre Aguayo <andre.aguayo@hotmail.com>
2023-04-03 10:59:45 -0300

    fix: request verification

--- core/src/Http/RequestValidate/ProductRequest.php
+++ core/src/Http/RequestValidate/ProductRequest.php
@@ -15,11 +15,15 @@ class ProductRequest
             $uriPaths[2] === 'create' &&
             $httpMethod === 'POST' &&
             ( //Product request params
+                !empty($request['name']) && //check product name
                 is_string($request['name']) && //check product name
+                !empty($request['value']) && //check value
                 is_numeric($request['value']) && //check value
                 $request['value'] >= 0 && //check value
+                !empty($request['stock']) && //check product stock
                 is_numeric($request['stock']) && //check product stock
                 $request['stock'] >= 0 && //check product stock
+                !empty($request['category_id']) && //check product_category_id
                 is_numeric($request['category_id']) //check product_category_id
             );
     }
